11a1573c1d7b88c4bce968ed9d9803d75c39c32a
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-05-27 17:03:29 +0530

    Update play.py

--- Mafia/modules/play.py
+++ Mafia/modules/play.py
@@ -46,8 +46,9 @@ from Mafia.services.callsmusic.callsmusic import client as USER
 from Mafia.services.converter.converter import convert
 from Mafia.services.downloaders import youtube
 
+aiohttpsession = aiohttp.ClientSession()
 chat_id = None
-arq = ARQ("https://thearq.tech", {ARQ_API_KEY})
+arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
 
 
 def cb_admin_check(func: Callable) -> Callable:
